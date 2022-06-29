7f31622f5e9c2fca7ff2a7c592dd8acd9490071d
jeevan.hs <jeevan.hs@thinktac.com>
2022-06-29 12:09:14 +0530

    removed phone number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-admin-lte-starter';
+  addclass: boolean = false;
+
+  ngOnInit(): void {
+    if (window.innerWidth < 658) {
+      this.addclass = true
+    } else {
+      this.addclass = false
+    }
+  }
+
+
 }
